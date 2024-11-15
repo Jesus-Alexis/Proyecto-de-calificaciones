6b85a1725f57bca9a589e9d99c9219e3e5f28162
Jesus Alexis <alexis.serrano.diaz56@gmail.com>
2024-11-14 18:50:11 -0600

    Primer commit

---
+++ C#/practica/Proyecto-guiado.cs
@@ -0,0 +1,25 @@
+ int Calificaciones = 5;
+ 
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahira1 = 84;
+int zahira2 = 96;
+int zahira3 = 73;
+int zahira4 = 85;
+int zahira5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
